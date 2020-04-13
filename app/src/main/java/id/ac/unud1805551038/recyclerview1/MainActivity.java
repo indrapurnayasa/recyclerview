319c06840ecbbe93e7f6c903fce2b3407626b996
indrapurnayasa <indra.purnayasa@student.unud.ac.id>
2020-04-13 15:37:30 +0800

    recyclerview

---
+++ app/src/main/java/id/ac/unud1805551038/recyclerview1/MainActivity.java
@@ -0,0 +1,31 @@
+package id.ac.unud1805551038.recyclerview1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.converse,R.drawable.vans,R.drawable.nike,R.drawable.adidas,R.drawable.newbalance,R.drawable.reebok,
+            R.drawable.compass,R.drawable.brodo};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.shoes_brand);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
